3750558cdf181f9b50e49858e8986c689c6511ed
joey patino <joseph.onitap@gmail.com>
2018-02-05 13:28:21 +0100

    Initial Commit

---
+++ SwiftVision/SwiftVision.h
@@ -0,0 +1,19 @@
+//
+//  SwiftVision.h
+//  SwiftVision
+//
+//  Created by Joey Patino on 2/5/18.
+//  Copyright © 2018 Joseph Patino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftVision.
+FOUNDATION_EXPORT double SwiftVisionVersionNumber;
+
+//! Project version string for SwiftVision.
+FOUNDATION_EXPORT const unsigned char SwiftVisionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftVision/PublicHeader.h>
+
+
